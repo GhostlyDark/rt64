d2e781b47b2021f3e4c8f5a40f49d5599a282764
GhostlyDark <31742919+GhostlyDark@users.noreply.github.com>
2026-08-24 20:31:14 +0200

    Revert "Consider the viewport clip rect for intersecting the draw area detection for triangles. (#262)"
    This reverts commit 5473732a822a4423b5696e7cb18fecc425a59875.

--- src/hle/rt64_rsp.cpp
+++ src/hle/rt64_rsp.cpp
@@ -1150,9 +1150,7 @@ namespace RT64 {
                 drawRect.lry = std::max(drawRect.lry, int32_t(hlslpp::ceil(v.y).x * 4.0f));
             }
 
-            const interop::RSPViewport &viewport = viewportStack[viewportStackSize - 1];
-            const FixedRect viewportClipRect = viewport.rect(clipRatios.data());
-            drawRect = viewportClipRect.intersection(scissorRect.intersection(drawRect));
+            drawRect = scissorRect.intersection(drawRect);
             if (!drawRect.isNull()) {
                 fbPair.drawColorRect.merge(drawRect);
                 if (otherModeStack[otherModeStackSize - 1].zUpd()) {
